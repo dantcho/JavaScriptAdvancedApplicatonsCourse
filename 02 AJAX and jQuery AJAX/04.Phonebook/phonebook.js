4f4249bcf5313c54d683390824a12558b4dae2a7
dantcho <danchoivanovv@gmail.com>
2016-11-20 01:25:04 +0200

    Added solution for FireFox

--- 02 AJAX and jQuery AJAX/04.Phonebook/phonebook.js
+++ 02 AJAX and jQuery AJAX/04.Phonebook/phonebook.js
@@ -1,4 +1,4 @@
-function attachEvents() {
+function attachEventsForChrome() {
     let btnLoad = $('#btnLoad').click(loadPhoneBook);
     let btnCreate = $('#btnCreate').click(createContact);
     let databaseString = 'https://phonebook-nakov.firebaseio.com/phonebook';
@@ -49,3 +49,58 @@ function attachEvents() {
     }
 
 }
+
+function attachEventsForFireFox() { //This is working solution for FireFox
+    let btnLoad = $('#btnLoad').click(loadPhoneBook);
+    let btnCreate = $('#btnCreate').click(createContact);
+    let databaseString = 'https://phonebook-nakov.firebaseio.com/phonebook';
+    //let databaseString = 'https://phonebook-c196d.firebaseio.com/';
+
+    let unordList = $('#phonebook');
+
+    function loadPhoneBook() {
+        $.get(databaseString + '.json')
+            .then(render)
+            .catch(displayError);
+    }
+
+    function displayError(err) {
+        $('#phonebook').text(err.statusText);
+    }
+
+    function render(data) {
+        unordList.empty();
+        for (let keyInTable in data) {
+            let insertLi =
+                $('<li>').addClass('phones').text(`${data[keyInTable].person}: ${data[keyInTable].phone} `)
+                    .append($('<button>').text('[Delete]').prop('baseId', keyInTable)
+                        .on('click', deleteContact));
+            unordList.append(insertLi);
+        }
+    }
+
+    function deleteContact(ev) {
+        let key = this.baseId;
+        let request = {
+            method: 'DELETE',
+            url: databaseString + '/' + key + '.json'
+        };
+        $.ajax(request)
+            .then(loadPhoneBook)
+            .catch(displayError);
+    }
+
+    function createContact() {
+        let newContactJSON = JSON.stringify({
+            person: $('#person').val(),
+            phone: $('#phone').val()
+        });
+        $.post(databaseString + '.json', newContactJSON)
+            .then(loadPhoneBook)
+            .catch(displayError);
+        $('#person').val('');
+        $('#phone').val('');
+    }
+
+}
+
